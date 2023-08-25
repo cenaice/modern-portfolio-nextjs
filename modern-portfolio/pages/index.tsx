ee935cd7c3445b6dde90b051286eafb3e0e433e7
Victor <victorphiathep@gmail.com>
2023-08-25 17:33:18 -0400

    Added Nextjs TailwindCSS files

---
+++ modern-portfolio/pages/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      {" "}
+      <Head>
+        {" "}
+        <title>Victer's Portfolio</title>{" "}
+      </Head>{" "}
+      <h1 className="p-19 text-red-500">Lets build this shit</h1> {/* Header */}{" "}
+      {/* Hero */} {/* About */} {/* Experiences?? */} {/* Skills */}{" "}
+      {/* Projects */} {/* Contact Me */}{" "}
+    </div>
+  );
+};
+export default Home;
