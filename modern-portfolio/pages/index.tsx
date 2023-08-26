aa6d0ba77ac6346ef1004d76ab422fe01f9c0d05
Victor <victorphiathep@gmail.com>
2023-08-26 00:37:51 -0400

    Added framer-motions and a header with react social icons

--- modern-portfolio/pages/index.tsx
+++ modern-portfolio/pages/index.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Header from "@/components/Header";
 
 const Home: NextPage = () => {
   return (
     <div>
-      {" "}
-      <Head>
-        {" "}
-        <title>Victer's Portfolio</title>{" "}
-      </Head>{" "}
-      <h1 className="p-19 text-red-500">Lets build this shit</h1> {/* Header */}{" "}
-      {/* Hero */} {/* About */} {/* Experiences?? */} {/* Skills */}{" "}
-      {/* Projects */} {/* Contact Me */}{" "}
+        <title>Victer's Portfolio</title>
+      {/* Header */}
+      <Header />
+      {/* Hero */}
+      {/* About */}
+      {/* Experiences?? */}
+      {/* Skills */}
+      {/* Projects */}
+      {/* Contact Me */}
     </div>
   );
 };
