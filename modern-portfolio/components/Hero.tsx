66617d0b450c74d10c24badb7c8e95dd71bb4a3f
Victer Phiathep <46116758+cenaice@users.noreply.github.com>
2023-08-28 13:18:02 -0400

    Finished backgroundCircle component

--- modern-portfolio/components/Hero.tsx
+++ modern-portfolio/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import LinkedInIMG from './images/linked-in.jpg'
 
 type Props = {};
 
@@ -16,10 +17,11 @@ export default function Hero({}: Props) {
   });
 
   return (
-    <div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden ">
       <BackgroundCircles />
+      <img src="linked-in.jpg"/>
       <h1>
-        <span className="relative flex justify-center items-center">{text}</span>
+        <span >{text}</span>
         <Cursor cursorColor="F7AB0A" />
       </h1>
     </div>
