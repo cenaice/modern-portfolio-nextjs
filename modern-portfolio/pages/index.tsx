95dd36dce51c6ce7215dbf13eab8363bc6141375
Victor <victorphiathep@gmail.com>
2023-09-05 22:38:57 -0400

    Added sanity.io

--- modern-portfolio/pages/index.tsx
+++ modern-portfolio/pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import type { NextPage } from "next";
-import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
@@ -11,7 +9,6 @@ import ContactMe from "@/components/ContactMe";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
       <title>Victer's Portfolio</title>
