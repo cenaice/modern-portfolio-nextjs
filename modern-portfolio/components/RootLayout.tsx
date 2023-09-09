ed3295159eb9a6d97069f6b973d3c54de9834a86
Victor <victorphiathep@gmail.com>
2023-09-09 17:47:18 -0400

    Removed last sanity.io file that was blocking build production

--- modern-portfolio/components/RootLayout.tsx
+++ modern-portfolio/components/RootLayout.tsx
@@ -1,71 +1,11 @@
-import React, { ReactNode } from "react";
-import type { NextPage } from "next";
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Experiences from "@/components/Experiences";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import ContactMe from "@/components/ContactMe";
-import Link from "next/link";
+import React from "react";
+import Header from "./Header"; // Import your header component
 
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+const RootLayout: React.FC<any> = ({ children }) => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
-      <title>Victer's Portfolio</title>
-      {/* Header */}
+    <div>
       <Header />
-      {/* Hero */}
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
-      {/* About */}
-      <section id="about" className="snap-center">
-        <About />
-      </section>
-
-      {/* Experiences?? 
-        <section id="experiences" className="snap-center">
-        <Experiences />
-        </section>   
-        */}
-      {/* Skills */}
-      <section id="skills" className="snap-start">
-        <Skills />
-      </section>
-      {/* Projects */}
-      <section id="projects" className="snap-start">
-        <Projects />
-      </section>
-      {/* Contact Me */}
-      <section id="contact" className="snap-start">
-        <ContactMe />
-      </section>
-
-      <Link href="#hero">
-        <footer className="sticky bottom-5 w-full cursor-pointer">
-          <div className="flex items-center justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className="h=7 w-7 rounded-full filter grayscale hover:grayscale-0"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
-              />
-            </svg>
-          </div>
-        </footer>
-      </Link>
+      <main>{children}</main>
     </div>
   );
 };
